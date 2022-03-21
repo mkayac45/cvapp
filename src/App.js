49bd0ce4949106a2c1b30d00f896c8815f39f53d
Mehmet <mkayac45@gmail.com>
2022-03-22 00:59:56 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Topbar from "./components/topbar/topbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Contact from "./components/contact/contact";
+import AboutMeComponent from "./components/aboutMeComponent/AboutMeComponent";
+import CvComponent from "./components/cvComponent/CvComponent";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Topbar></Topbar>
+      <BrowserRouter>
+      <Routes>
+        <Route exact  path="/" element={<jobHistory />}/>
+          <Route exact  path="about" element={<AboutMeComponent />} />
+          <Route exact  path="contact" element={<Contact />} />
+          <Route exact  path="cvComponent" element={<CvComponent />} />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/cvComponent/CvComponent.jsx
@@ -0,0 +1,33 @@
+const  CvComponent=() => {
+    return(
+<div>
+
+
+<h1>Kişisel Bilgiler</h1>
+<ul>
+<li class="list-group-item">Ad-Soyad : Mehmet KAYACI</li>
+<li class="list-group-item">Doğum Tarihi : 16/12/1999</li>
+<li class="list-group-item">Lise : İbrahim Bodur Anadolu Lisesi</li>
+<li class="list-group-item">Üniversite : Trakya Üniversitesi Bilgisayar Mühendisliği</li>
+</ul>
+
+<h1>Bilgisayar Dilleri</h1>
+<ul>
+<li class="list-group-item">Java --- İyi</li>
+<li class="list-group-item">Python --- İyi</li>
+<li class="list-group-item">Css/Html --- Başlangıç</li>
+<li class="list-group-item">SQL(MySQL) --- Orta </li>
+</ul>
+
+<h1>İlgi Alanları</h1>
+<ul>
+<li class="list-group-item">Bilgisayar Oyunları</li>
+<li class="list-group-item">Kamp</li>
+<li class="list-group-item">Doğa Yürüyüşü</li>
+<li class="list-group-item">Atıcılık</li>
+</ul>
+    
+</div>
+)
+}
+export default CvComponent;
