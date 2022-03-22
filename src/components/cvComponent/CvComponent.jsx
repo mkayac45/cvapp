72f166fd1dacfc553175705c65f638e367d793f8
Mehmet <mkayac45@gmail.com>
2022-03-22 05:50:46 +0300

    cvcomponent commit

--- src/components/cvComponent/CvComponent.jsx
+++ src/components/cvComponent/CvComponent.jsx
@@ -1,33 +1,152 @@
+import "./CvComponent.css";
 const  CvComponent=() => {
     return(
 <div>
+    <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous"></link>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css" />
+    <link rel="stylesheet" href="CvComponent.css" />
+    </head>
+    <body class = "bg-light">
 
 
-<h1>Kişisel Bilgiler</h1>
-<ul>
-<li class="list-group-item">Ad-Soyad : Mehmet KAYACI</li>
-<li class="list-group-item">Doğum Tarihi : 16/12/1999</li>
-<li class="list-group-item">Lise : İbrahim Bodur Anadolu Lisesi</li>
-<li class="list-group-item">Üniversite : Trakya Üniversitesi Bilgisayar Mühendisliği</li>
-</ul>
+        <div class="container p-5">
+            <div class= "row">
+                    <div class="col-lg-4 bg-dark text-white text-center py-4">
+                        <div class = "header-left">
+                            <img src="https://media-exp1.licdn.com/dms/image/C4D03AQFfL8rNfv_kIQ/profile-displayphoto-shrink_200_200/0/1565302041919?e=1648080000&v=beta&t=MLsJsgXk8qcpdSNm_-p1_GvRhzUwQulSdybsfaojFjA" 
+                            alt="" class="img-thumbnail rounded-circle mb-2"></img>
+                            <h1 class="display-5">Mehmet Kayacı</h1>
+                        </div>
 
-<h1>Bilgisayar Dilleri</h1>
-<ul>
-<li class="list-group-item">Java --- İyi</li>
-<li class="list-group-item">Python --- İyi</li>
-<li class="list-group-item">Css/Html --- Başlangıç</li>
-<li class="list-group-item">SQL(MySQL) --- Orta </li>
-</ul>
+                        <div>
+                            <h5 class="text-uppercase bg-white text-dark py2 rounded-pill">Contact</h5>
 
-<h1>İlgi Alanları</h1>
-<ul>
-<li class="list-group-item">Bilgisayar Oyunları</li>
-<li class="list-group-item">Kamp</li>
-<li class="list-group-item">Doğa Yürüyüşü</li>
-<li class="list-group-item">Atıcılık</li>
-</ul>
-    
-</div>
+                            <u1 class="list-unstyled text-white-50 ml-5 py-2 text-left">
+                                <li class= "list-item">
+                                    <i class="fas fa-mobile-alt mx-4"></i>
+                                    +905********
+                                </li>
+
+                                <li class= "list-item">
+                                    <i class="fas fa-envelope-open-text mx-4"></i>
+                                    mail@gmail.com
+                                </li>
+
+                                <li class= "list-item">
+                                    <i class="fas fa-blog mx-4"></i>
+                                    <a href="https://www.linkedin.com/in/mehmet-kayaci-948974190/">Linkedin</a>
+                                </li>
+
+                                <li class= "list-item">
+                                    <i class="fas fa-blog mx-4"></i>
+                                    <a href="https://github.com/mkayac45">Github</a>
+                                </li>
+
+                                <li class= "list-item">
+                                    <i class="fas fa-map-marker-alt mx-4"></i>
+                                    Edirne/Türkiye
+                                </li>
+
+                                <li class= "list-item">
+                                    <i class="fas  fa-calendar"></i>
+                                    16/12/1999
+                                </li>
+
+                                <li class= "list-item">
+                                    <i class="fas  fa-location-pin"></i>
+                                    Uşak
+                                </li>
+
+                                <li class= "list-item">
+                                    <i class="fas fa-mars-and-venus"></i>
+                                    Erkek
+                                </li>
+
+                            </u1>
+            
+                        </div>
+                        <div>
+                            
+                            <h5 class="text-uppercase bg-white text-dark py2 rounded-pill">Programing Languages</h5>
+                            <span> Python</span>
+                            <div class="box">
+                            <div class="InnerBox">
+                            </div>
+                            </div>
+
+                            <span> Java</span>
+                            <div class="box">
+                            <div class="InnerBox2">
+                            </div>
+                            </div>
+
+                            <span> C# </span>
+                            <div class="box">
+                            <div class="InnerBox3">
+                            </div>
+                            </div>
+
+                            <span> C++</span>
+                            <div class="box">
+                            <div class="InnerBox4">
+                            </div>
+                            </div>
+                            
+                            <span>HTMLCSS</span>
+                            <div class="box">
+                            <div class="InnerBox5">
+                            </div>
+                            </div>
+                           
+
+                        
+                          
+                            
+                            
+                        </div>
+                        <div>
+                        <h5 class="text-uppercase bg-white text-dark py2 rounded-pill">Hobiler</h5>
+                        <u1 class="list-unstyled text-white-50 ml-5 py-2 text-left ">
+                                <li class= "list-item"><i class="fa-solid fa-campground"></i>Kamp</li>
+                                <li class= "list-item"><i class="fa-solid fa-gamepad"></i>Strateji Oyunları</li>
+                                <li class= "list-item"><i class="fa-solid fa-person-walking"></i>Doğa Yürüyüşü</li>
+                                <li class= "list-item"><i class="fa-duotone fa-gun"></i>Atıcılık</li>
+                                <li class= "list-item"><i class="fa-solid fa-dumbbell"></i>Gym</li>
+                                
+                            </u1>
+                        
+
+                    </div>
+
+                    </div>
+                <div class="col-lg-8 bg-light text-dark text-center py-4 px-5">
+                    <div class= "header-right" >
+                        <h4 class="text-uppercase">ÖzGeçmiş</h4>
+                        <p>
+                        1999 yılında Uşak/Sivaslı ilçesinde doğdum.İlk okulu Çanakkale/Çan ilçesinde okudum.Sivas/Altınyayla ilçesinde ortaokulu okudum daha sonrasında tekrar Çanakkaleye gelerek orta
+        ortaokul ve lise eğitimimi bitirdim.Üniversite eğitimimi halen Trakya Üniversitesinde devam ettiriyorum.
+                        </p>
+                        
+                    </div>
+
+
+                    <div>
+                    <h4 class="text-uppercase">İş Alanım</h4>
+                        <p>
+                        Küçüklükten itibaren sürekli bilgisayarla uğraşmışımdır hatta bir çok bilgisayarımı da bu yüzden kurcalayıp bozmuşumdur.En genel ilgi alanım aslında bilgisayar.
+        Çoğu kişi gibi bu alana oyun yapmak veya hacker olmak hayalleriyle girdim.Tabi ki oyun geliştirme denemelerim oldu ama bu alan beni çok tatmin etmedi.Daha sonrasında
+        siber güvenliğe yöneldim hem donanım olarak hem de yazılımsal olarak uğraşabileceğim alanlardan bir tanesiydi ve ilgimi çekiyordu.Şuanda da bu alanda devam ediyorum. 
+                        </p>
+                        
+
+                    </div>
+
+                </div>
+            </div>       
+    </div>
+    </body>
+    </div>
 )
 }
 export default CvComponent;
